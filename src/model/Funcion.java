92aef7f7e913f326ea936192e8b35bf6d49b701c
rorro <rorro@Rodrigo-PC>
2017-11-09 18:25:58 -0300

    Creo clase abstracta funcion de dos variables

--- src/model/Funcion.java
+++ src/model/Funcion.java
@@ -2,4 +2,6 @@ package model;
 
 public abstract class Funcion {
 
+	
+	
 }
---
+++ src/model/FuncionDosVariables.java
@@ -0,0 +1,11 @@
+package model;
+
+public abstract class FuncionDosVariables extends Funcion{
+	
+	private double parametro1;
+	private double parametro2;
+	
+	public abstract double evaluar(double parametro1,double parametro2);
+}
+
+
