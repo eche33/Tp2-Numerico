32873613edb66b45cef66e44525f24e2fcca35e8
rorro <rorro@Rodrigo-PC>
2017-11-09 18:58:06 -0300

    Funcion de Euler explicito

---
+++ src/model/FuncionEulerExplicito.java
@@ -0,0 +1,19 @@
+package model;
+
+/*
+ * Funcion obtenida para punto 1 del tp
+ * La funcion es  -0.0000406*(T-889.79)
+ */
+
+public class FuncionEulerExplicito extends Funcion {
+
+	
+	@Override
+	public double evaluar(double parametro) {
+		
+		double resultado = -0.000406*(parametro-889.79);
+		
+		return resultado;
+	}
+
+}
---
+++ src/tests/FuncionEulerExplicitoTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+import model.FuncionEulerExplicito;
+
+class FuncionEulerExplicitoTests {
+
+	@Test
+	void funcionEvaluaCorrectamenteConParametrosBasicos() {
+		
+		FuncionEulerExplicito funcion = new FuncionEulerExplicito();
+		
+		Assert.assertEquals(0.360, funcion.evaluar(1), 0.001);
+	}
+	
+	@Test
+	void funcionEvaluaCorrectamente2() {
+		
+		FuncionEulerExplicito funcion = new FuncionEulerExplicito();
+		
+		Assert.assertEquals(0, funcion.evaluar(889.79), 0);
+	}
+
+}
