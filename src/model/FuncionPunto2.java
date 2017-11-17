d22c479d590750be51f1259d32b0238e92513345
rorro <rorro@192.168.1.9>
2017-11-17 12:32:02 -0300

    Funcion punto 2 arreglada

--- src/model/FuncionPunto2.java
+++ src/model/FuncionPunto2.java
@@ -1,12 +1,12 @@
 package model;
 
 /*
- * f = 0.000406.(t-889) - 9.79E-13 (t^4-889^4)
+ * f = 0.000406.(t-889) - 9.79E-13 (t^4-tinf^4)
  */
 
 public class FuncionPunto2 extends Funcion {
 
-	@Override
+	/*@Override
 	public double evaluar(double parametro) {
 		
 		double parteConveccion = -0.000406*(parametro-889);
@@ -17,6 +17,28 @@ public class FuncionPunto2 extends Funcion {
 		double parteRadiacion = -0.000000000000979*(parametroElevado-temperaturaInicialElevada);
 		
 		return (parteConveccion+parteRadiacion);
+	}*/
+	
+	public FuncionPunto2 (double largoTotal, double velocidadInicial, double t1, double t2) {
+		
+		this.largoTotal = largoTotal;
+		this.velocidadInicial = velocidadInicial;
+		this.t1 = t1;
+		this.t2 = t2;
+	}
+
+	@Override
+	public double evaluar(double parametro, double tiempo) {
+		
+		double parteConveccion = -0.000406*(parametro-this.determinarTemperatura(tiempo));
+		
+		double parametroElevado = Math.pow(parametro, 4);
+		double temperaturaHornoElevada = Math.pow(this.determinarTemperatura(tiempo), 4);
+		
+		double parteRadiacion = -0.000000000000979*(parametroElevado-temperaturaHornoElevada);
+		
+		return (parteConveccion+parteRadiacion);
+		
 	}
 	
 	
--- src/tests/FuncionPunto2Tests.java
+++ src/tests/FuncionPunto2Tests.java
@@ -12,27 +12,27 @@ class FuncionPunto2Tests {
 	@Test
 	void funcionEvaluaCorrectamenteConCero() {
 		
-		FuncionPunto2 funcion = new FuncionPunto2();
+		FuncionPunto2 funcion = new FuncionPunto2(50, 0.0342, 889, 889);
 		
-		Assert.assertEquals(0.97, funcion.evaluar(0), 0.01);
+		Assert.assertEquals(0.97, funcion.evaluar(0, 700), 0.01);
 	}
 	
 	
 	@Test
 	void funcionEvaluaCorrectamenteConValorInicial() {
 		
-		FuncionPunto2 funcion = new FuncionPunto2();
+		FuncionPunto2 funcion = new FuncionPunto2(50, 0.0342, 889, 889);
 		
-		Assert.assertEquals(0, funcion.evaluar(889), 0.01);
+		Assert.assertEquals(0, funcion.evaluar(889, 700), 0.01);
 	}
 	
 	
 	@Test
 	void funcionEvaluaCorrectamenteConDiez() {
 		
-		FuncionPunto2 funcion = new FuncionPunto2();
+		FuncionPunto2 funcion = new FuncionPunto2(50, 0.0342, 889, 889);
 		
-		Assert.assertEquals(0.96, funcion.evaluar(10), 0.01);
+		Assert.assertEquals(0.96, funcion.evaluar(10, 700), 0.01);
 	}
 
 }
