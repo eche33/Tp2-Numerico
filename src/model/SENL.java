00d14f0f4701899d7ca34342a2fac6634035c422
rorro <rorro@192.168.1.9>
2017-11-20 14:15:24 -0300

    SENL

--- src/model/SENL.java
+++ src/model/SENL.java
@@ -10,14 +10,14 @@ public class SENL {
 	private double jacobiano[] [];
 	private F1 F1;
 	private F2 F2;
-	private MetodoED metodo;
 	private double soakingObj;
 	private double tempObj;
 	
 	
-	public SENL(MetodoED metodo) {
+	public SENL(double soakingObj, double tempObj) {
 		
-		this.metodo = metodo;
+		this.soakingObj = soakingObj;
+		this.tempObj = tempObj;
 		this.jacobiano = new double [2][2];
 		this.F1 = new F1(this.soakingObj);
 		this.F2 = new F2(this.tempObj);
@@ -34,6 +34,36 @@ public class SENL {
 		this.jacobiano[0][1] = 1.5;
 		this.jacobiano[1][0] = 1.5;
 		this.jacobiano[1][1] = -0.5;
+		
+	}
+	
+	
+	
+	public double[] multiplicarMatrices(double[][] matrizA, double []matrizB) {
+		
+		double[] resultado =new double [2];
+		
+		for (int i = 0; i<2; i++) {
+			
+			double parcial = 0;
+			
+			for (int j = 0; j<2; j++) {
+				
+				parcial = parcial + ((matrizA[i][j])*(matrizB[j]));
+				
+			}
+			
+		resultado [i] = parcial;	
+		}
+		
+		return resultado;
+			
+		
+		
+	}
+	
+	public void calcular(double T1Semilla, double T2Semilla) {
+		
 		
 	}
 	
